628823014b6c1c437e320ef3c76d2e411af4fcd7
Eyad Farah <45307338+eyadartist@users.noreply.github.com>
2021-01-27 18:58:28 +0200

    Add files via upload

---
+++ Egypt FWD/landing page/app.js
@@ -0,0 +1,44 @@
+window.addEventListener("load", () => {
+    // let seclct the nav list
+    const navList = document.getElementById("nav_list")
+    //  we put fragment in order to make a best preformance for the code
+    const fragment = document.createDocumentFragment();
+    //  let select all sections
+    const sections = document.querySelectorAll("section")
+    // let select the ul
+    const ui = document.querySelectorAll("ul")
+
+    // let make a loop
+    sections.forEach((sec) => {
+        // let get the data name from the HTML
+        let data = sec.getAttribute("data-nav")
+        // let create li
+        let li = document.createElement("li")
+        // let get the text from the data-nav
+        let text = document.createTextNode(data)
+        // after that we add menu-link class
+        li.classList.add("menu-link")
+        // let create the link
+        let a = document.createElement("a")
+        // let add event in order to mke the page scroll to acho
+
+        // Make a smooth scroll
+        a.addEventListener("click", () => {
+            sec.scrollIntoView({ behavior: 'smooth' })
+        })
+
+        a.appendChild(text)
+        li.appendChild(a)
+        fragment.appendChild(li)
+    })
+
+    navList.appendChild(fragment)
+
+})
+
+// open and close menu 
+
+function toggle() {
+    var menu = document.getElementById("nav_list")
+    menu.classList.toggle("show")
+}
